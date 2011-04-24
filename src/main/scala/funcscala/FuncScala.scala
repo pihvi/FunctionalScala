6b94795dd19bf84a2282ba44ae8fd8ff30d07098
Antti Mattila <antti.mattila@reaktor.fi>
2011-04-24 15:19:42 +0300

    Add length

--- src/main/scala/funcscala/FuncScala.scala
+++ src/main/scala/funcscala/FuncScala.scala
@@ -28,18 +28,24 @@ sealed abstract class Lst[+A] {
     case (s, e)          => s +: interval(s + step, e, step)
   }
 
-  def insertAt[A](index: Int, x: A, lst: Lst[A]): Lst[A] = lst match {
-    case _ if (index == 0) => x +: lst;
-    case a +: as           => a +: insertAt(index - 1, x, as)
+  def insertAt[A](index: Int, a: A, lst: Lst[A]): Lst[A] = lst match {
+    case _ if (index == 0) => a +: lst;
+    case x +: xs           => x +: insertAt(index - 1, a, xs)
     case Nl                => Nl
+  }
+
+  def length[A](lst: Lst[A]): Int = lst match {
+    case Nl      => 0
+    case x +: xs => 1 + length(xs)
   }
   //def append[A](a: A, lst: Lst[A]): Lst[A] = insertAt(length(lst), a, lst)
 }
 
 object FuncScala extends Application {
-  println("index out of bounds: " + Nl.insertAt(6, 0, 1 +: 2 +: 3 +: Nl))
-  println(Nl.insertAt(1, 0, 1 +: 2 +: 3 +: Nl))
+  println(Nl.length(1 +: 2 +: Nl))
   println("Start time: " + new java.util.Date)
+  //  println("index out of bounds: " + Nl.insertAt(6, 0, 1 +: 2 +: 3 +: Nl))
+  //  println(Nl.insertAt(1, 0, 1 +: 2 +: 3 +: Nl))
   //  println(1 +: 2 +: Nl)
   //  println(Nl.repeat("2", 8))
   //  println(Nl.interval(start = 3, end = 27, step = 3))
